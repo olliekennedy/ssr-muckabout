223a033afef507a0a4d9cb8371d363b1c52b7ea2
Ollie Kennedy <ollie.kennedy1@gmail.com>
2025-07-21 09:09:36 +0100

    add a simple initial test

--- src/test/kotlin/com/olliekennedy/HelloWorldTest.kt
+++ src/test/kotlin/com/olliekennedy/HelloWorldTest.kt
@@ -1,21 +1,15 @@
 package com.olliekennedy
 
-import com.natpryce.hamkrest.and
 import com.natpryce.hamkrest.assertion.assertThat
-import com.natpryce.hamkrest.equalTo
-import org.http4k.core.Body
-import org.http4k.core.ContentType.Companion.TEXT_HTML
+import com.natpryce.hamkrest.contains
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status.Companion.OK
-import org.http4k.hamkrest.hasBody
-import org.http4k.hamkrest.hasHeader
-import org.http4k.hamkrest.hasQuery
-import org.http4k.hamkrest.hasStatus
-import org.http4k.lens.string
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class HelloWorldTest {
+
+    @Test
+    fun `shows the title`() {
+        assertThat(app.invoke(Request(GET, "/")).bodyString(), contains(Regex("<h1>Simple SSR Calculator</h1>")))
+    }
 }
