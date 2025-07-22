de4aa90a070d9ebc32139a595bca29cb03f2ad8b
Ollie Kennedy <ollie.kennedy1@gmail.com>
2025-07-22 21:59:49 +0100

    filter out some more non-public stations

--- src/main/kotlin/com/olliekennedy/StationParser.kt
+++ src/main/kotlin/com/olliekennedy/StationParser.kt
@@ -3,6 +3,7 @@ package com.olliekennedy
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.io.InputStream
+import kotlin.collections.map
 
 class StationParser {
 
@@ -12,14 +13,31 @@ class StationParser {
             .use { it.readText() }
             .let { Json.decodeFromString<StationDataset>(it) }
             .CORPUS
+            .filterOutUndesirables()
+            .map { Station(code = it.`3ALPHA`, name = it.NLCDESC) }
+
+    private fun getInputStreamFrom(filename: String): InputStream =
+        (javaClass.getResourceAsStream(filename)
+            ?: error("$filename not found in resources!"))
+
+    private fun List<StationData>.filterOutUndesirables(): List<StationData> =
+        this
             .filter { it.`3ALPHA`.isNotBlank() }
             .filter { !it.`3ALPHA`.startsWith("X") }
             .filter { it.NLCDESC.isNotBlank() }
             .filter { it.NLCDESC != "." }
-            .map { Station(code = it.`3ALPHA`, name = it.NLCDESC) }
-
-    private fun getInputStreamFrom(filename: String): InputStream = (javaClass.getResourceAsStream(filename)
-        ?: error("$filename not found in resources!"))
+            .filter { !it.NLCDESC.contains("DEPOT") }
+            .filter { !it.NLCDESC.contains("STORE") }
+            .filter { !it.NLCDESC.contains("SIDING") }
+            .filter { !it.NLCDESC.contains("YARD") }
+            .filter { !it.NLCDESC.contains("WORKS") }
+            .filter { !it.NLCDESC.contains("MAINT") }
+            .filter { !it.NLCDESC.contains("LOCOMOTIVE") }
+            .filter { !it.NLCDESC.contains("REPAIR") }
+            .filter { !it.NLCDESC.contains("BUS S") }
+            .filter { !it.NLCDESC.contains("SLIPWAY") }
+            .filter { !it.NLCDESC.contains("Metrolink") }
+            .filter { !it.NLCDESC.contains("MTLK") }
 
     companion object {
         @Suppress("PropertyName")
