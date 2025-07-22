4e30cc0dd904d2b6179fd6b9582881699e7030fc
Ollie Kennedy <ollie.kennedy1@gmail.com>
2025-07-22 22:03:51 +0100

    move to one kt file for simplicity

--- src/main/kotlin/com/olliekennedy/LastTrainHome.kt
+++ src/main/kotlin/com/olliekennedy/LastTrainHome.kt
@@ -1,5 +1,7 @@
 package com.olliekennedy
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import org.http4k.core.Body
 import org.http4k.core.ContentType.Companion.TEXT_HTML
 import org.http4k.core.Filter
@@ -26,6 +28,7 @@ import org.http4k.server.asServer
 import org.http4k.template.HandlebarsTemplates
 import org.http4k.template.ViewModel
 import org.http4k.template.viewModel
+import java.io.InputStream
 import java.util.UUID
 
 data class Station(
@@ -127,6 +130,61 @@ val app: HttpHandler = routes(
 
     "/static" bind static(Classpath("/public")),
 )
+
+class StationParser {
+
+    fun parse(filename: String): List<Station> =
+        getInputStreamFrom(filename)
+            .bufferedReader()
+            .use { it.readText() }
+            .let { Json.Default.decodeFromString<StationDataset>(it) }
+            .CORPUS
+            .filterOutUndesirables()
+            .map { Station(code = it.`3ALPHA`, name = it.NLCDESC) }
+
+    private fun getInputStreamFrom(filename: String): InputStream =
+        (javaClass.getResourceAsStream(filename)
+            ?: error("$filename not found in resources!"))
+
+    private fun List<StationData>.filterOutUndesirables(): List<StationData> =
+        this
+            .filter { it.`3ALPHA`.isNotBlank() }
+            .filter { !it.`3ALPHA`.startsWith("X") }
+            .filter { it.NLCDESC.isNotBlank() }
+            .filter { it.NLCDESC != "." }
+            .filter { !it.NLCDESC.contains("DEPOT") }
+            .filter { !it.NLCDESC.contains("STORE") }
+            .filter { !it.NLCDESC.contains("SIDING") }
+            .filter { !it.NLCDESC.contains("YARD") }
+            .filter { !it.NLCDESC.contains("WORKS") }
+            .filter { !it.NLCDESC.contains("MAINT") }
+            .filter { !it.NLCDESC.contains("LOCOMOTIVE") }
+            .filter { !it.NLCDESC.contains("REPAIR") }
+            .filter { !it.NLCDESC.contains("BUS S") }
+            .filter { !it.NLCDESC.contains("SLIPWAY") }
+            .filter { !it.NLCDESC.contains("Metrolink") }
+            .filter { !it.NLCDESC.contains("MTLK") }
+
+    companion object {
+        @Suppress("PropertyName")
+        @Serializable
+        data class StationDataset(
+            val CORPUS: List<StationData>,
+        )
+
+        @Suppress("PropertyName")
+        @Serializable
+        data class StationData(
+            val NLC: Int,
+            val STANOX: String,
+            val TIPLOC: String,
+            val `3ALPHA`: String,
+            val UIC: String,
+            val NLCDESC: String,
+            val NLCDESC16: String,
+        )
+    }
+}
 
 fun main() {
     val printingApp: HttpHandler = SessionFilter().then(PrintRequest().then(app))
